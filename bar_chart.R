4371cc5a4eb18b569f6659d966362bf671246998
GeorgeSnape <c0022646@ncl.ac.uk>
2023-10-28 09:54:10 +0100

    Add scatter plot of height vs mass

---
+++ bar_chart.R
@@ -0,0 +1,13 @@
+# install.packages("dplyr")
+library(dplyr)
+data("starwars")
+glimpse(starwars)
+
+# install.packages("ggplot2")
+library(ggplot2)
+
+# create a bar plot counting no. of species
+starwars |> 
+  count(species) |> 
+  ggplot(aes(y = species, x = n)) +
+  geom_col()
